565c63f261fa7e9bdae75bf334c9fc3c2b4f1b60
yann-lcrx <yannick84v@gmail.com>
2022-01-02 21:19:47 +0100

    feat: prevent interaction with modal background

--- caption.js
+++ caption.js
@@ -1,7 +1,9 @@
 document.getElementById("help").addEventListener("click", () => {
-  document.getElementById("help-tooltip").classList.add("visible");
+  document.getElementById("help-wrapper").classList.add("visible");
+  document.getElementById("canvas-zone").classList.add("blur");
 });
 
 document.getElementById("help-close").addEventListener("click", () => {
-  document.getElementById("help-tooltip").classList.remove("visible");
+  document.getElementById("help-wrapper").classList.remove("visible");
+  document.getElementById("canvas-zone").classList.remove("blur");
 });
