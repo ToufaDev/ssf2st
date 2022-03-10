f27eac3268a28ab697a9ceef13bf9856d279be0e
Antoine <Toufadev>
2022-03-10 21:13:45 +0100

    fix typo on japanese translation

--- scripts/build_attacks.sh
+++ scripts/build_attacks.sh
@@ -18,16 +18,28 @@ echo "" > attacks.html
 
 echo "        <div class=\"categories\">" >> attacks.html
 echo "          <div class=\"tab toggled\" id=\"tab-1\">" >> attacks.html
-echo "            <p>Normals</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"en\" >Normals</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"fr\" >Normaux</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"alt\" >Normals</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"jp\" >通常技</p>" >> attacks.html
 echo "          </div>" >> attacks.html
 echo "          <div class=\"tab\" id=\"tab-2\">" >> attacks.html
-echo "            <p>Aerials</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"en\" >Aerials</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"fr\" >Aerials</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"alt\" >Aerials</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"jp\" >空中攻撃</p>" >> attacks.html
 echo "          </div>" >> attacks.html
 echo "          <div class=\"tab\" id=\"tab-3\">" >> attacks.html
-echo "            <p>Specials</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"en\">Specials</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"fr\">Speciaux</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"alt\">Specials</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"jp\">必殺技</p>" >> attacks.html
 echo "          </div>" >> attacks.html
 echo "          <div class=\"tab\" id=\"tab-4\">" >> attacks.html
-echo "            <p>Misc</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"en\">Misc</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"fr\">Divers</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"alt\">Misc</p>" >> attacks.html
+echo "            <p style=\"display: none;\" lang=\"jp\">雑</p>" >> attacks.html
 echo "          </div>" >> attacks.html
 echo "        </div>" >> attacks.html
 echo "        <div class=\"moves\">" >> attacks.html
@@ -50,47 +62,40 @@ for move in $(echo */ | tr -d "/"); do
     move_name_en=${dic_en[$dic_entry]}
     move_name_alt=${dic_alt[$dic_entry]}
     move_name_fr=${dic_fr[$dic_entry]}
-    move_name_es=${dic_es[$dic_entry]}
     move_name_jp=${dic_jp[$dic_entry]}
 
     move_name_en=$(echo "${move_name_en^}")
     move_name_alt=$(echo "${move_name_alt^}")
     move_name_fr=$(echo "${move_name_fr^}")
-    move_name_es=$(echo "${move_name_es^}")
     move_name_jp=$(echo "${move_name_jp^}")
     
     line_en=$(echo "            <li><a style=\"display: none;\" lang=\"en\" href='#"$move"'>"${move_name_en}"</a></li>" | tr " " "?")
     line_alt=$(echo "            <li><a style=\"display: none;\" lang=\"alt\" href='#"$move"'>"${move_name_alt}"</a></li>" | tr " " "?")
     line_fr=$(echo "            <li><a style=\"display: none;\" lang=\"fr\" href='#"$move"'>"${move_name_fr}"</a></li>" | tr " " "?")
-    line_es=$(echo "            <li><a style=\"display: none;\" lang=\"es\" href='#"$move"'>"${move_name_es}"</a></li>" | tr " " "?")
     line_jp=$(echo "            <li><a style=\"display: none;\" lang=\"jp\" href='#"$move"'>"${move_name_jp}"</a></li>" | tr " " "?")
 
     if [[ $move =~ 00[0-9][0-9]-.* ]]; then
 	normals+=$line_en
 	normals+=$line_alt
 	normals+=$line_fr
-	normals+=$line_es
 	normals+=$line_jp
 	normals+=" "
     elif [[ $move =~ 01[0-9][0-9]-.* ]]; then
 	aerials+=$line_en
 	aerials+=$line_alt
 	aerials+=$line_fr
-	aerials+=$line_es
 	aerials+=$line_jp
 	aerials+=" "
     elif [[ $move =~ 1[0-9][0-9][0-9]-.* ]]; then
 	specials+=$line_en
 	specials+=$line_alt
 	specials+=$line_fr
-	specials+=$line_es
 	specials+=$line_jp
 	specials+=" "
     else
 	misc+=$line_en
 	misc+=$line_alt
 	misc+=$line_fr
-	misc+=$line_es
 	misc+=$line_jp
 	misc+=" "
     fi
