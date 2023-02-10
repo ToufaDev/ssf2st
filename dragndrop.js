0f9278ee1a9d36aa6a33c426e42a03c8af356543
Antoine <Toufadev>
2023-02-10 21:03:45 +0900

    for mobile

--- dragndrop.js
+++ dragndrop.js
@@ -21,26 +21,26 @@ function add2p() {
 } 
 
 function onMouseoverEvt(evt) {
-selectedImage = this.src;
-var imgid = this.id;
-$('#addimagemenu').show();
-$('#addimagemenu').position({
-    my: "center",
-    at: "center",
-    of: "#" + imgid
-}).show();
+    selectedImage = this.src;
+    var imgid = this.id;
+    $('#addimagemenu').show();
+    $('#addimagemenu').position({
+	my: "center",
+	at: "center",
+	of: "#" + imgid
+    }).show();
 }
 
 function getParameterByName(name, url) {
-if (!url) {
-    url = window.location.href;
-}
-name = name.replace(/[\[\]]/g, "\$&");
-var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-if (!results) return null;
-if (!results[2]) return '';
-return decodeURIComponent(results[2].replace(/\+/g, " "));
+    if (!url) {
+	url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 $(function() {
@@ -50,11 +50,11 @@ $(function() {
 var index = window.location.href.indexOf('?');
 if(index != -1){
     var querystring = window.location.href.slice(index + 1)       
-            $('a[class="fixquery"]').each(function() {
-                var $this = $(this);       
-                var href = $this.attr("href");
-                href += (href.indexOf('?') != -1) ? '&' : '?';
-                href += querystring;
-                $this.attr("href", href);
-            });
-    }
+    $('a[class="fixquery"]').each(function() {
+        var $this = $(this);       
+        var href = $this.attr("href");
+        href += (href.indexOf('?') != -1) ? '&' : '?';
+        href += querystring;
+        $this.attr("href", href);
+    });
+}
--- imageEdit.js
+++ imageEdit.js
@@ -2,13 +2,9 @@ document
     .getElementById('zoomin')
     .addEventListener('click', function() {
         if (document.getElementById('canvas-zone').classList.contains('zoomedIn')) {
-            document
-                .getElementById('canvas-zone')
-                .classList.remove('zoomedIn')
-                return ''
+            document.getElementById('canvas-zone').classList.remove('zoomedIn')
+            return ''
         }
-        document
-            .getElementById('canvas-zone')
-            .classList.add('zoomedIn')
+        document.getElementById('canvas-zone').classList.add('zoomedIn')
     })
 
